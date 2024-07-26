2e399d94b7eb164173fc30bffb4f2b7f53386090
rizkyy20 <muhammadrizkyyamin@gmail.com>
2024-07-26 10:48:02 +0800

    For My portofolio

---
+++ script.js
@@ -0,0 +1,100 @@
+function tes() {
+    const mycheck = document.getElementById("box")
+    const kotak = document.querySelector(".title")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes2() {
+    const mycheck = document.getElementById("box2")
+    const kotak = document.querySelector(".title2")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes3() {
+    const mycheck = document.getElementById("box3")
+    const kotak = document.querySelector(".title3")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes4() {
+    const mycheck = document.getElementById("box4")
+    const kotak = document.querySelector(".title4")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes5() {
+    const mycheck = document.getElementById("box5")
+    const kotak = document.querySelector(".title5")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes6() {
+    const mycheck = document.getElementById("box6")
+    const kotak = document.querySelector(".title6")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes7() {
+    const mycheck = document.getElementById("box7")
+    const kotak = document.querySelector(".title7")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes8() {
+    const mycheck = document.getElementById("box8")
+    const kotak = document.querySelector(".title8")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes9() {
+    const mycheck = document.getElementById("box9")
+    const kotak = document.querySelector(".title9")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
+function tes0() {
+    const mycheck = document.getElementById("box0")
+    const kotak = document.querySelector(".title0")
+    if (mycheck.checked) {
+        kotak.style.textDecoration = "line-through"
+        mycheck.disabled = true;
+    }
+}
+
+
